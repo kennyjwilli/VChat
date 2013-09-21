a9f517a752f2c50e27139fe30ddedf69e4284a33
Kenny Williams <kenny@vectorgaming.net>
2013-09-20 21:46:09 -0700

    Added sub command to allow the joining of channels

---
+++ src/net/vectorgaming/vchat/commands/user/ChannelJoin.java
@@ -0,0 +1,67 @@
+
+package net.vectorgaming.vchat.commands.user;
+
+import net.vectorgaming.vchat.ChatManager;
+import net.vectorgaming.vchat.VChatAPI;
+import net.vectorgaming.vchat.framework.channel.Channel;
+import net.vectorgaming.vcore.framework.commands.SubCommand;
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+/**
+ *
+ * @author Kenny
+ */
+public class ChannelJoin extends SubCommand
+{
+    public ChannelJoin()
+    {
+        super("join", VChatAPI.getPlugin());
+    }
+    
+    @Override
+    public void run(CommandSender cs, String[] args)
+    {
+        Player p = (Player) cs;
+        
+        if(!ChatManager.channelExists(args[0]))
+        {
+            this.sendErrorMessage(cs, "Channel "+ChatColor.YELLOW+args[0]+ChatColor.RED+" does not exist.");
+            return;
+        }
+        Channel channel = ChatManager.getChannel(args[0]);
+        ChatManager.focusChannel(p, args[0]);
+        cs.sendMessage(ChatColor.translateAlternateColorCodes('&', ChatColor.GREEN+"Focused on "+channel.getColor()+channel.getName()));
+    }
+
+    @Override
+    public String getDescription()
+    {
+        return "Joins a channel";
+    }
+
+    @Override
+    public String getUsage()
+    {
+        return "/ch join <channel>";
+    }
+
+    @Override
+    public String getPermission()
+    {
+        return "vchat.join";
+    }
+
+    @Override
+    public Integer getMinArgsLength()
+    {
+        return 1;
+    }
+
+    @Override
+    public boolean isPlayerOnlyCommand()
+    {
+        return true;
+    }
+}
