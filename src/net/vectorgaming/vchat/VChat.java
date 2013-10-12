57262aa8ddf6358eda1ee86f243fc4ed39273527
Kenny Williams <kenny@vectorgaming.net>
2013-10-11 19:14:02 -0700

    Fixed bug where players are not added to their respective channels when the server reloads

--- src/net/vectorgaming/vchat/VChat.java
+++ src/net/vectorgaming/vchat/VChat.java
@@ -1,6 +1,8 @@
 package net.vectorgaming.vchat;
 
 import net.vectorgaming.vchat.commands.user.ChannelCommand;
+import net.vectorgaming.vchat.framework.channel.Channel;
+import net.vectorgaming.vchat.framework.channel.SLChannel;
 import net.vectorgaming.vchat.framework.channel.type.BasicChannelCreator;
 import net.vectorgaming.vchat.listeners.ChatListener;
 import net.vectorgaming.vchat.listeners.PlayerJoinListener;
@@ -10,6 +12,7 @@ import net.vectorgaming.vcore.framework.VertexAPI;
 import net.vectorgaming.vcore.framework.VertexPlugin;
 import net.vectorgaming.vcore.framework.commands.CommandManager;
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginManager;
 
@@ -33,6 +36,7 @@ public class VChat extends VertexPlugin
         registerChannels();
         SLAPI.loadAllChannels();
         SLAPI.loadJoinedChannels();
+        loadJoinedPlayers();
     }
 
     @Override
@@ -77,5 +81,20 @@ public class VChat extends VertexPlugin
     private void registerChannels()
     {
         VChatAPI.registerChannelType("BASIC_CHANNEL", new BasicChannelCreator());
+    }
+
+    private void loadJoinedPlayers()
+    {
+        for(Player p : Bukkit.getOnlinePlayers())
+        {
+            for(Channel c : ChatManager.getChannels())
+            {
+                if(c instanceof SLChannel)
+                {
+                    if(((SLChannel) c).getAllPlayers().contains(p.getName())) ChatManager.focusChannel(p, c);
+                }
+
+            }
+        }
     }
 }
