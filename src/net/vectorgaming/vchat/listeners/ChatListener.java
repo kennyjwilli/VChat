4156dc2170292a0d139656d085427d9741be7dcc
Kenny Williams <kenny@vectorgaming.net>
2013-12-01 14:23:44 -0800

    Catches any exceptions with the chat event and notifies the player that there seems to be an issue.

--- src/net/vectorgaming/vchat/listeners/ChatListener.java
+++ src/net/vectorgaming/vchat/listeners/ChatListener.java
@@ -1,11 +1,13 @@
 
 package net.vectorgaming.vchat.listeners;
 
+import net.arcanerealm.arcanelib.ArcaneTools;
 import net.vectorgaming.vchat.ChatManager;
 import net.vectorgaming.vchat.framework.channel.Channel;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 
@@ -15,7 +17,7 @@ import org.bukkit.event.player.AsyncPlayerChatEvent;
  */
 public class ChatListener implements Listener
 {
-    @EventHandler
+    @EventHandler(priority = EventPriority.MONITOR)
     public void onChatEvent(final AsyncPlayerChatEvent event)
     {
         final Player p = event.getPlayer();
@@ -28,8 +30,15 @@ public class ChatListener implements Listener
         }
         
         Channel channel = ChatManager.getFocusedChannel(p);
-        
-        channel.onChat(p, event.getMessage());
+        try
+        {
+            channel.onChat(p, event.getMessage());
+        }catch(Exception e)
+        {
+            p.sendMessage(ChatColor.RED+"Uh oh! There seems to be a problem with the chat. "
+                    + "If you see a crazy developer tell them this: "+ArcaneTools.getStackTraceError(e.getStackTrace()));
+            e.printStackTrace();
+        }
         event.setCancelled(true);
     }
 }
