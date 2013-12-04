844d32181641751c17cd9f343a33f54620d4888b
Kenny Williams <kenny@vectorgaming.net>
2013-12-03 21:09:08 -0800

    Added several methods to sort through the avaliable channel types.
    Added javadocs

--- src/net/vectorgaming/vchat/VChatAPI.java
+++ src/net/vectorgaming/vchat/VChatAPI.java
@@ -2,6 +2,7 @@
 package net.vectorgaming.vchat;
 
 import java.util.HashMap;
+import java.util.Set;
 import net.vectorgaming.vchat.framework.channel.ChannelCreator;
 import net.vectorgaming.vcore.framework.VertexPlugin;
 
@@ -11,7 +12,7 @@ import net.vectorgaming.vcore.framework.VertexPlugin;
  */
 public class VChatAPI
 {
-    private static HashMap<String, ChannelCreator> creator = new HashMap<>();
+    private static final HashMap<String, ChannelCreator> creator = new HashMap<>();
     private static VChat plugin;
     
     public VChatAPI(VChat instance) 
@@ -19,18 +20,53 @@ public class VChatAPI
         plugin = instance;
     }
     
+    /**
+     * Gets the plugin
+     * @return Plugin
+     */
     public static VertexPlugin getPlugin()
     {
         return plugin;
     }
     
+    /**
+     * Registers a channel creator so that the channel can be created via the in-game
+     * command
+     * @param type The type of the channel. This is generally all upper case and spaces
+     * should be replaced with underscores
+     * @param creator The channel creator class for the channel type
+     */
     public static void registerChannelType(String type, ChannelCreator creator)
     {
         VChatAPI.creator.put(type, creator);
     }
     
+    /**
+     * Gets the channel creator for a given channel type
+     * @param type Type of channel
+     * @return A channel creator object
+     */
     public static ChannelCreator getChannelCreator(String type)
     {
         return creator.get(type);
+    }
+    
+    /**
+     * Checks to see if the channel type has been registered
+     * @param type Type of channel
+     * @return true if the channel type exists
+     */
+    public static boolean channelTypeExists(String type)
+    {
+        return creator.containsKey(type);
+    }
+    
+    /**
+     * Returns a list of channel types that are avaliable
+     * @return List of channel types
+     */
+    public static Set<String> getChannelTypes()
+    {
+        return creator.keySet();
     }
 }
